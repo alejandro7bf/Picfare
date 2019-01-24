b173c921570a75cfd6d6657b546b31af0a52e90d
Alejandro <Maeuvemo@MacBook-Pro-Vibeship.local>
2019-01-24 18:20:49 -0500

    error Solved
    Discovered one error that the storyboards wasnt in the same reference folder than the rest of the project. so the mainstoryboard and the launchsreen wasnt being saved in git. Solved

--- Picfare/Controllers/Detail/DetailViewController.swift
+++ Picfare/Controllers/Detail/DetailViewController.swift
@@ -11,8 +11,8 @@ import WebKit
 class DetailViewController: UIViewController, WKNavigationDelegate {
 
     var movie = Movie()
-  
     let URL_PREFIX = "https://image.tmdb.org/t/p/w200/"
+    
     @IBOutlet weak var Titlelabel: UILabel!
     @IBOutlet weak var Overviewtext: UITextView!
     @IBOutlet weak var Image: UIImageView!
@@ -25,43 +25,45 @@ class DetailViewController: UIViewController, WKNavigationDelegate {
     @IBOutlet weak var Star3: UIButton!
     @IBOutlet weak var Star4: UIButton!
     @IBOutlet weak var Star5: UIButton!
-    var stars: [UIButton] = []
+    lazy var stars: [UIButton] = [Star1,Star2,Star3,Star4,Star5]
 
     @IBOutlet weak var VoteCount: UILabel!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-       stars.append(Star1)
-        stars.append(Star2)
-        stars.append(Star3)
-        stars.append(Star4)
-        stars.append(Star5)
         
+      
         self.Activity.transform = CGAffineTransform(scaleX: 1.6, y: 1.6)
         video.navigationDelegate = self
         self.video.addSubview(self.Activity)
         self.Activity.startAnimating()
-       
-        load_stars()
-        movie.load_video { (response) in
-            if !(response.isEmpty) {
-            let URLvideo: URL = NSURL(string: "https://www.youtube.com/embed/" + response )! as URL
-            let myrequest = URLRequest(url: URLvideo)
-            self.video.load(myrequest)
-            }
-       }
+        load_video()
+
         
-     
+        load_stars()
+        
+ 
         Titlelabel.text = movie.title!
         Overviewtext.text = movie.overview!
-        
         let urlimage = NSURL(string: URL_PREFIX + movie.poster_path!)! as URL
         Image.load_image(url: urlimage)
     
 
     }
     
+    
+    
+    
+    func load_video(){
+        movie.load_video { (response) in
+            if !(response.isEmpty) {
+                let URLvideo: URL = NSURL(string: "https://www.youtube.com/embed/" + response )! as URL
+                let myrequest = URLRequest(url: URLvideo)
+                self.video.load(myrequest)
+            }
+        }
+    }
     
     
     func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
