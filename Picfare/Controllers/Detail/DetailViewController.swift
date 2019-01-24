bdd38afc5344f7b097f34baf8e4f060ab6c5233f
Alejandro <Maeuvemo@MacBook-Pro-Vibeship.local>
2019-01-24 11:29:26 -0500

    Stars
    Rating stars were added

--- Picfare/Controllers/Detail/DetailViewController.swift
+++ Picfare/Controllers/Detail/DetailViewController.swift
@@ -13,61 +13,85 @@ class DetailViewController: UIViewController, WKNavigationDelegate {
     var movie = Movie()
   
     let URL_PREFIX = "https://image.tmdb.org/t/p/w200/"
-   
     @IBOutlet weak var Titlelabel: UILabel!
-
     @IBOutlet weak var Overviewtext: UITextView!
-    
     @IBOutlet weak var Image: UIImageView!
-    
-    
-    
     @IBOutlet weak var video: WKWebView!
  
     @IBOutlet weak var Activity: UIActivityIndicatorView!
     
+    @IBOutlet weak var Star1: UIButton!
+    @IBOutlet weak var Star2: UIButton!
+    @IBOutlet weak var Star3: UIButton!
+    @IBOutlet weak var Star4: UIButton!
+    @IBOutlet weak var Star5: UIButton!
+    var stars: [UIButton] = []
+
+    @IBOutlet weak var VoteCount: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-       self.Activity.transform = CGAffineTransform(scaleX: 1.6, y: 1.6)
+       stars.append(Star1)
+        stars.append(Star2)
+        stars.append(Star3)
+        stars.append(Star4)
+        stars.append(Star5)
         
-        
+        self.Activity.transform = CGAffineTransform(scaleX: 1.6, y: 1.6)
         video.navigationDelegate = self
-        
         self.video.addSubview(self.Activity)
         self.Activity.startAnimating()
        
-      
-        
+        load_stars()
         movie.load_video { (response) in
             if !(response.isEmpty) {
             let URLvideo: URL = NSURL(string: "https://www.youtube.com/embed/" + response )! as URL
             let myrequest = URLRequest(url: URLvideo)
             self.video.load(myrequest)
-           
             }
-        
-     
        }
         
-      
      
         Titlelabel.text = movie.title!
         Overviewtext.text = movie.overview!
         
-     
         let urlimage = NSURL(string: URL_PREFIX + movie.poster_path!)! as URL
         Image.load_image(url: urlimage)
-        
     
-        
-        
 
     }
+    
+    
     
     func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
       self.Activity.stopAnimating()
     }
     
+    
+    
+    func load_stars(){
+        let number: Double = movie.vote_average!/2
+        let Decimalpart: Double = number - Double(Int(number))
+        VoteCount.text = "(" + String(movie.vote_count!) + ")"
+
+        let index: Int = Int(number)
+        var i = 0
+        while i < index {
+            
+            stars[i].setImage(UIImage(named: "ImageFull"), for: UIControl.State.normal)
+            
+            if i + 1 == index {
+                if Decimalpart > 0.3 && Decimalpart < 0.8{
+                    stars[i + 1].setImage(UIImage(named: "StarHalf"), for: UIControl.State.normal)
+                }else if Decimalpart >= 0.8 {
+                    stars[i+1].setImage(UIImage(named: "ImageFull"), for: UIControl.State.normal)
+                }
+            }
+            i = i + 1
+        
+        }
+    }
     
     
 
